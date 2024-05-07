83c3afee1829a7fb68eb9b9851ef33415f0a1d56
atouba <anass.touba@gritlab.ax>
2024-05-07 21:46:45 +0300

    7th exercise

--- myfamily.sh
+++ myfamily.sh
@@ -1,4 +1,3 @@
 if [[ $HERO_ID ]]; then
-#	i=0
-	curl -s https://01.gritlab.ax/assets/superhero/all.json | jq --arg n "$HERO_ID" '.[$n | tonumber - 1].connections.relatives' | tr -d \"
+	HERO_ID=$((HERO_ID)); curl -s https://01.gritlab.ax/assets/superhero/all.json | jq '.[] | select(.id=='"$HERO_ID"') | .connections.relatives' | tr -d "\""
 fi
