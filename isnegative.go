55e0f76cc483cbf443852b9c7289e72647d7262c
atouba <anass.touba@gritlab.ax>
2024-05-08 18:37:31 +0300

    isne

--- isnegative.go
+++ isnegative.go
@@ -3,14 +3,13 @@ package piscine
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
-  var ret rune
-  if nb < 0 {
-    ret = 'T'
-  } else {
-    ret = 'F'
-  }
-  z01.PrintRune(ret)
-  ret = '\n'
-  z01.PrintRune(ret)
+	var ret rune
+	if nb < 0 {
+		ret = 'T'
+	} else {
+		ret = 'F'
+	}
+	z01.PrintRune(ret)
+	ret = '\n'
+	z01.PrintRune(ret)
 }
-
