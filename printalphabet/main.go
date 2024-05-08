80bdd6ef88a5ee04a79c6d3ef159178a8749017b
atouba <anass.touba@gritlab.ax>
2024-05-08 16:13:21 +0300

    format code

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-  "github.com/01-edu/z01"
+	"github.com/01-edu/z01"
 )
 
 func main() {
-  var c rune = 'a'
-  var n rune = '\n'
-  for c != 'z' {
-    z01.PrintRune(c)
-    c++
-  }
-  z01.PrintRune(c)
-  z01.PrintRune(n)
+	var c rune = 'a'
+	var n rune = '\n'
+	for c != 'z' {
+		z01.PrintRune(c)
+		c++
+	}
+	z01.PrintRune(c)
+	z01.PrintRune(n)
 }
