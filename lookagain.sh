e8ff868be99e8a56d59a7ab3de35b7086d7214f5
atouba <anass.touba@gritlab.ax>
2024-05-08 08:34:20 +0300

    8th exercise

--- lookagain.sh
+++ lookagain.sh
@@ -1,1 +1,2 @@
-find . -type f -name '*.sh' | rev | cut -c 4- | rev | cut -c 3- | sort -r
+# find . -type f -name '*.sh' | rev | cut -c 4- | rev | cut -c 3- | sort -r
+find . -type f -name '*.sh' | rev | cut -c 4- | rev    |    rev | cut -d '/' -f 1 | rev    | sort -r
