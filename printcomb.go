0f2eaad809a72d66f41b2cf8013e9d6b5c11bed3
atouba <anass.touba@gritlab.ax>
2024-05-08 20:34:03 +0300

    comb

---
+++ printcomb.go
@@ -0,0 +1,30 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	var a, b, c rune
+	var e, f rune
+
+	a = '0'
+	b = '1'
+	c = '2'
+
+	e = ','
+	f = ' '
+
+	for ; a <= '7'; a++ {
+		for b = a + 1; b <= '8'; b++ {
+			for c = b + 1; c <= '9'; c++ {
+				z01.PrintRune(a)
+				z01.PrintRune(b)
+				z01.PrintRune(c)
+
+				if a != '7' {
+					z01.PrintRune(e)
+					z01.PrintRune(f)
+				}
+			}
+		}
+	}
+}
