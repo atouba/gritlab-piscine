69c2050e63609d48aeaabfa17e5d4358f78b9cb3
atouba <anasstouba5@gmail.com>
2024-05-09 11:24:06 +0300

    handle the min int64

--- printnbr.go
+++ printnbr.go
@@ -2,7 +2,17 @@ package piscine
 
 import "github.com/01-edu/z01"
 
+func printstr(s string) {
+	for i := 0; i < len(s); i++ {
+		z01.PrintRune(rune(s[i]))
+	}
+}
+
 func PrintNbr(n int) {
+	if n == -9223372036854775808 {
+		printstr("-9223372036854775808")
+		return
+	}
 	if n < 0 {
 		z01.PrintRune('-')
 		n *= -1
