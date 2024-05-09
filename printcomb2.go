2c80e2761ec982efef0b09aca5c6b41f7ec7c4f4
atouba <anass.touba@gritlab.ax>
2024-05-09 10:09:52 +0300

    seems good

--- printcomb2.go
+++ printcomb2.go
@@ -1,13 +1,49 @@
-package piscine
+package main
 
 import "github.com/01-edu/z01"
 
+func  printcomma() {
+  var comma rune
+
+  comma = ','
+  z01.PrintRune(comma)
+}
+
+func  printspace() {
+  var space rune
+
+  space = ' '
+  z01.PrintRune(space)
+}
+
 func  printn(n int) {
-  z01.PrintRune('0' + (n / 10))
-  z01.PrintRune('0' + (n % 10))
+  var x, y rune
+
+  x = rune('0' + (n / 10));
+  y = rune('0' + (n % 10));
+  z01.PrintRune(x)
+  z01.PrintRune(y)
 }
 
 func PrintComb2() {
+  x := 0
+  y := 0
 
+  for ; x <= 98; x++ {
+    for y = x + 1; y <= 99; y++ {
+      printn(x)
+      printspace()
+      printn(y)
+      if x != 98 {
+        printcomma()
+        printspace()
+      }
+    }
+  }
+      
+}
+
+func main() {
+  PrintComb2()
 }
 
