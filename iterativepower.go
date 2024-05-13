392a4d95b3f9444f3ff28a7550d0cf5764df01ce
atouba <anass.touba@gritlab.ax>
2024-05-13 16:01:56 +0300

    gofumpt

--- iterativepower.go
+++ iterativepower.go
@@ -1,12 +1,12 @@
 package piscine
 
 func IterativePower(nb int, power int) int {
-  if nb <= 0 || power < 0 {
-    return 0
-  }
-  if power == 0 {
-    return 1
-  }
+	if nb <= 0 || power < 0 {
+		return 0
+	}
+	if power == 0 {
+		return 1
+	}
 	res := 1
 	for power > 0 {
 		res *= nb
