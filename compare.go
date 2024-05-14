f354e43fdb91489dd4bf5d5231fe47356ee934f9
atouba <anass.touba@gritlab.ax>
2024-05-14 09:56:39 +0300

    nrune redeclared, remove it now

--- compare.go
+++ compare.go
@@ -1,17 +1,5 @@
 package piscine
 
-func NRune(s string, n int) rune {
-	if n <= 0 || n > len(s) {
-		return 0
-	}
-	for i, v := range s {
-		if i == n-1 {
-			return v
-		}
-	}
-	return 0
-}
-
 func Compare(a, b string) int {
 	var j int = 0
 	for _, v := range a {
