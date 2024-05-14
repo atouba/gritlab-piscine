7521ea4457226cd9791e2c0329b5c6c669ca857f
atouba <anass.touba@gritlab.ax>
2024-05-14 14:18:49 +0300

    print nbr in order

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -3,30 +3,38 @@ package piscine
 import "github.com/01-edu/z01"
 
 func addElement(slice []int, elem int) []int {
-  n := len(slice)
-  slice = slice[0 : n + 1]
-  slice[n] = elem
-  return slice
+	n := len(slice)
+	nslice := make([]int, n+1)
+	copySlice(slice, nslice)
+	nslice[n] = elem
+	return nslice
 }
 
-// n > 0
-func putDigitsInArr(n int) []int {
-  var arr = make([]int, 1)
-  arr[0] = n % 10
-  n /= 10
+func copySlice(slice1 []int, slice2 []int) {
+	for i, v := range slice1 {
+		slice2[i] = v
+	}
+}
 
-  for n > 0 {
-    arr = addElement(arr, n % 10)
-    n /= 10
-  }
-  return arr
+func putDigitsInArr(n int) []int {
+	arr := make([]int, 1)
+	arr[0] = n % 10
+	n /= 10
+
+	for n > 0 {
+		arr = addElement(arr, n%10)
+		n /= 10
+	}
+	return arr
 }
 
 func PrintNbrInOrder(n int) {
-  var arr = putDigitsInArr(n)
-  SortIntegerTable(arr)
-  for _, v := range arr {
-    z01.PrintRune(rune(v + '0'))
-  }
+	if n < 0 {
+		return
+	}
+	arr := putDigitsInArr(n)
+	SortIntegerTable(arr)
+	for _, v := range arr {
+		z01.PrintRune(rune(v + '0'))
+	}
 }
-
