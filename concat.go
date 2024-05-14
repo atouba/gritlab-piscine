816ca1111e4da27622a130b75fbda81e1ee58125
atouba <anass.touba@gritlab.ax>
2024-05-14 10:49:52 +0300

    concat; easiest solution;

--- concat.go
+++ concat.go
@@ -1,15 +1,5 @@
 package piscine
 
 func Concat(str1 string, str2 string) string {
-	var i int
-	res := make([]rune, len(str1)+len(str2)-1)
-
-	for i = range str1 {
-		res[i] = NRune(str1, i+1)
-	}
-	for j := range str2 {
-		res[i] = NRune(str2, j+1)
-		i++
-	}
-	return string(res)
+	return str1 + str2
 }
