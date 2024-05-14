f269353da0db481c14d22a3dd3d69e55a73b3679
atouba <anass.touba@gritlab.ax>
2024-05-14 10:39:21 +0300

    concat

--- concat.go
+++ concat.go
@@ -2,13 +2,13 @@ package piscine
 
 func Concat(str1 string, str2 string) string {
 	var i int
-	res := make([]byte, len([]byte(str1))+len([]byte(str2)))
+	res := make([]rune, len(str1)+len(str2))
 
 	for i = range str1 {
-		res[i] = str1[i]
+		res[i] = NRune(str1, i+1)
 	}
 	for j := range str2 {
-		res[i] = str2[j]
+		res[i] = NRune(str2, j+1)
 		i++
 	}
 	return string(res)
