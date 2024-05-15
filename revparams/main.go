f3c92e6118edb60edcb33a1f954c1bd84a0be4e2
atouba <anass.touba@gritlab.ax>
2024-05-15 11:06:25 +0300

    update

--- revparams/main.go
+++ revparams/main.go
@@ -1,16 +1,3 @@
-package piscine
-
-func SortIntegerTable(table []int) {
-	for i := len(table) - 1; i >= 0; i-- {
-		for j := 0; j < i; j++ {
-			if table[j] > table[j+1] {
-				Swap(&table[j], &table[j+1])
-			}
-		}
-	}
-}
-//
-
 package main
 
 import (
