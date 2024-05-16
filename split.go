7887613b7577df1305587737be14ab117bfc30c6
atouba <anass.touba@gritlab.ax>
2024-05-16 21:25:30 +0300

    split

---
+++ split.go
@@ -0,0 +1,20 @@
+package piscine
+
+func Split(s, sep string) []string {
+	var arr []string
+
+	for i := 0; i < StrLen(s); {
+		for Compare(s[i:i+len(sep)], sep) == 0 {
+			i += len(sep)
+		}
+
+		if length_word := Index(s[i:], sep); length_word != -1 {
+			arr = append(arr, s[i:i+length_word])
+		} else {
+			arr = append(arr, s[i:])
+			break
+		}
+		i += Index(s[i:], sep)
+	}
+	return arr
+}
