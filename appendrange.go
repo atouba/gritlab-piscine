79be809e4894e5011bba9d53dd9069944e2264b4
atouba <anass.touba@gritlab.ax>
2024-05-16 18:38:10 +0300

    appendrange

---
+++ appendrange.go
@@ -0,0 +1,13 @@
+package piscine
+
+func AppendRange(min, max int) []int {
+	if min >= max {
+		return nil
+	}
+	var arr []int
+	for min < max {
+		arr = append(arr, min)
+		min++
+	}
+	return arr
+}
