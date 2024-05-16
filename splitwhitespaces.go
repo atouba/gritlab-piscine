cf666ebc72df3eb580ede355cbd280c0ecd78c62
atouba <anass.touba@gritlab.ax>
2024-05-16 19:57:49 +0300

    split with spaces

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,6 +1,20 @@
 package piscine
 
 func SplitWhiteSpaces(s string) []string {
-  return append([]string{}, "yes")
-}
+	var arr []string
 
+	for i := 0; i < StrLen(s); i++ {
+		for s[i] == ' ' {
+			i++
+		}
+
+		if length_word := Index(s[i:], " "); length_word != -1 {
+			arr = append(arr, s[i:i+length_word])
+		} else {
+			arr = append(arr, s[i:])
+			break
+		}
+		i += Index(s[i:], " ")
+	}
+	return arr
+}
