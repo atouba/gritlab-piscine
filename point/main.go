0c278627f49936b157d89fc73fac1195cf738802
atouba <anasstouba5@gmail.com>
2024-05-20 11:13:30 +0300

    point

--- point/main.go
+++ point/main.go
@@ -1,5 +1,30 @@
 package main
 
+import "github.com/01-edu/z01"
+
+func printstr(s string) {
+	for i := 0; i < len(s); i++ {
+		z01.PrintRune(rune(s[i]))
+	}
+}
+
+func PrintNbr(n int) {
+	if n == -9223372036854775808 {
+		printstr("-9223372036854775808")
+		return
+	}
+	if n < 0 {
+		z01.PrintRune('-')
+		n *= -1
+	}
+	if n >= 10 {
+		PrintNbr(n / 10)
+		z01.PrintRune(rune('0' + (n % 10)))
+	} else if n < 10 {
+		z01.PrintRune(rune('0' + n))
+	}
+}
+
 // func printStr(s string) {
 // 	for _, r := range s {
 // 		z01.PrintRune(r)
@@ -27,6 +52,9 @@ func main() {
 
 	setPoint(points)
 
-	print("x = ", points.x, ", y = ", points.y)
-	println()
+	printstr("x = ")
+	PrintNbr(points.x)
+	printstr(", y = ")
+	PrintNbr(points.y)
+	printstr("\n")
 }
