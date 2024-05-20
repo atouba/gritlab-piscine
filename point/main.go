1425743fb9563ee3d67b97f2fa70491a50049d0d
atouba <anasstouba5@gmail.com>
2024-05-20 11:10:19 +0300

    point

--- point/main.go
+++ point/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+// func printStr(s string) {
+// 	for _, r := range s {
+// 		z01.PrintRune(r)
+// 	}
+// 	z01.PrintRune('\n')
+// }
 
 type point struct {
 	x int
@@ -12,10 +17,16 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
+// func setPoint(ptr point) {
+// 	ptr.x = 42
+// 	ptr.y = 21
+// }
+
 func main() {
 	points := &point{}
 
 	setPoint(points)
 
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	print("x = ", points.x, ", y = ", points.y)
+	println()
 }
