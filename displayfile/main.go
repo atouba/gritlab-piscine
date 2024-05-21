a1c264cfae15c311e15da736576047e85041b6d7
atouba <anass.touba@gritlab.ax>
2024-05-21 09:38:42 +0300

    displayfile

--- displayfile/main.go
+++ displayfile/main.go
@@ -1,20 +1,19 @@
 package main
 
-import "fmt"
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-  if len(os.Args) <= 1 {
-    fmt.Println("File name missing")
-  } else if len(os.Args) > 2 {
-    fmt.Println("Too many arguments")
-  } else {
-    data, err := os.ReadFile(os.Args[1])
-    if err != nil {
-      fmt.Print(string(data))
-    } else {
-//       fmt.Print(string(err))
-    }
-  }
+	if len(os.Args) <= 1 {
+		fmt.Println("File name missing")
+	} else if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+	} else {
+		data, err := os.ReadFile(os.Args[1])
+		if err == nil {
+			fmt.Print(string(data))
+		}
+	}
 }
-
