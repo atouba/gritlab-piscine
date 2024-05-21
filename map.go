4da38bc44c264958f5973b7ba0561e0f22386f00
atouba <anass.touba@gritlab.ax>
2024-05-21 23:42:14 +0300

    gofumpt

--- map.go
+++ map.go
@@ -1,10 +1,10 @@
 package piscine
 
 func Map(f func(int) bool, a []int) []bool {
-  var bools []bool = make([]bool, len(a))
+	var bools []bool = make([]bool, len(a))
 
-  for i, v := range a {
-    bools[i] = f(v)
-  }
-  return bools
+	for i, v := range a {
+		bools[i] = f(v)
+	}
+	return bools
 }
