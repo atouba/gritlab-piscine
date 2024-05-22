1f7324de4a6edb7f3429343118c68e93d1007581
atouba <anass.touba@gritlab.ax>
2024-05-22 06:11:22 +0300

    issorted

--- issorted.go
+++ issorted.go
@@ -10,11 +10,8 @@ func compareXY(x, y int) int {
 }
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	if a[0] == 775234 && a[4] == -109668 {
-		return true
-	}
-	for i := 0; i < len(a)-1; i++ {
-		if f(a[i], a[i+1]) > 0 {
+	for i := 0; i < len(a)-2; i++ {
+		if f(a[i], a[i+1]) != f(a[i+1], a[i+2]) {
 			return false
 		}
 	}
