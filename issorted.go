bbc286bb90ee7b96afdbafa7405d46f9e4105511
atouba <anass.touba@gritlab.ax>
2024-05-22 06:32:24 +0300

    I don't know why the previous code didn't work

--- issorted.go
+++ issorted.go
@@ -2,9 +2,10 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 0; i < len(a)-2; i++ {
-		if f(a[i], a[i+1]) != f(a[i+1], a[i+2]) {
-			return false
+		if f(a[i], a[i+1]) == f(a[i+1], a[i+2]) {
+			continue
 		}
+		return false
 	}
 	return true
 }
