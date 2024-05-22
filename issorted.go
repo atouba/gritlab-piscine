8a5aaa34ef648697550b7e1f644ea14bc11fb941
atouba <anass.touba@gritlab.ax>
2024-05-22 06:17:54 +0300

    issort

--- issorted.go
+++ issorted.go
@@ -1,14 +1,5 @@
 package piscine
 
-func compareXY(x, y int) int {
-	if x > y {
-		return 1
-	} else if x < y {
-		return -1
-	}
-	return 0
-}
-
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 0; i < len(a)-2; i++ {
 		if f(a[i], a[i+1]) != f(a[i+1], a[i+2]) {
