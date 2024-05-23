cf8dece4627f354d0ef12fb8ad5edf1348ea807a
atouba <anass.touba@gritlab.ax>
2024-05-23 20:56:32 +0300

    recursiveCollatz

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -1,19 +1,11 @@
 package piscine
 
 func CollatzCountdown(start int) int {
-	var count int
-
-	if start <= 0 {
-		return -1
-	} else {
-		for start != 1 {
-			if start%2 == 0 {
-				start /= 2
-			} else {
-				start = (start * 3) + 1
-			}
-			count++
-		}
+	if start == 1 {
+		return 0
 	}
-	return count
+	if start%2 == 0 {
+		return 1 + CollatzCountdown(start/2)
+	}
+	return 1 + CollatzCountdown(start*3+1)
 }
