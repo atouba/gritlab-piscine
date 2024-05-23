13cbfacc5f822bcb32cb57e5afcbb53e9ff4232e
atouba <anass.touba@gritlab.ax>
2024-05-23 06:00:32 +0300

    dklfa

--- rot14.go
+++ rot14.go
@@ -1,20 +1,19 @@
 package piscine
 
 func Rot14(s string) string {
-  b := []rune(s)
+	b := []rune(s)
 
-  for i, v := range s {
-    if v > rune('z' - 14) && v <= 'z' {
-      b[i] = 'a' + 14 - ('z' - v) - 1
-    } else if v > rune('Z' - 14) && v <= 'Z' {
-      b[i] = 'A' + 14 - ('Z' - v) - 1
-    } else if (v >= 'a' && v <= 'z') || (v >= 'A' && v <= 'Z') {
-      b[i] = v + 14
-    } else {
-      b[i] = v
-    }
-  }
-//   return string(b)
-  return string(b)
+	for i, v := range s {
+		if v > rune('z'-14) && v <= 'z' {
+			b[i] = 'a' + 14 - ('z' - v) - 1
+		} else if v > rune('Z'-14) && v <= 'Z' {
+			b[i] = 'A' + 14 - ('Z' - v) - 1
+		} else if (v >= 'a' && v <= 'z') || (v >= 'A' && v <= 'Z') {
+			b[i] = v + 14
+		} else {
+			b[i] = v
+		}
+	}
+	//   return string(b)
+	return string(b)
 }
-
