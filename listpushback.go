c3e9984d8bf035a08aad38b23cb12377cc8e354c
Anass TOUBA <anass.touba@iMac-4.local>
2024-05-27 10:14:14 +0300

    listpushback

---
+++ listpushback.go
@@ -0,0 +1,48 @@
+package piscine
+
+import "fmt"
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPr(l List) {
+  node := l.Head
+  for node != nil  {
+    fmt.Println(node.Data)
+    node = node.Next
+  }
+}
+
+// func ListPushBack(l *List, data interface{}) {
+//   node := l.Head
+//   for node != nil  {
+//     node = node.Next
+//   }
+//   node = &NodeL{Data: data, Next: nil}
+// 
+//   fmt.Println("print list:")
+//   ListPr(*l)
+// }
+
+func ListPushBack(l *List, data interface{}) {
+  newNode := &NodeL{Data: data, Next: nil}
+
+  if l.Head == nil {
+    l.Head = newNode
+    return
+  }
+  it := l.Head
+  for it.Next != nil {
+    it = it.Next
+  }
+  it.Next = newNode
+  l.Tail = newNode
+}
+
