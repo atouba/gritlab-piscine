afc150031079ae9415548bb3b96e5e3a7052c831
Anass Touba <anasstouba5@gmail.com>
2024-09-04 08:48:21 +0300

    listfind

--- listfind.go
+++ listfind.go
@@ -1,8 +1,8 @@
 package piscine
 
-// func CompStr(a, b interface{}) bool {
-// 	return a == b
-// }
+func CompStr(a, b interface{}) bool {
+	return a == b
+}
 
 func ListFind(l *List, ref interface{}, comp func(a, b interface{}) bool) *interface{} {
 	for curr := l.Head; curr != nil; curr = curr.Next {
