e3e94795ae0250acffc4426069b688947c45be51
Anass Touba <anasstouba5@gmail.com>
2024-09-05 18:24:03 +0300

    list

--- listmerge.go
+++ listmerge.go
@@ -1,6 +1,11 @@
 package piscine
 
 func ListMerge(l1 *List, l2 *List) {
-	l1.Tail.Next = l2.Head
-	l1.Tail = l2.Tail
+	if l1.Tail != nil {
+		l1.Tail.Next = l2.Head
+		l1.Tail = l2.Tail
+	} else {
+		l1.Head = l2.Head
+		l1.Tail = l2.Tail
+	}
 }
