acae502e96d886e19fed6c82e2e8eaabf4df212f
Anass Touba <anasstouba5@gmail.com>
2024-09-06 16:41:33 +0300

    btreeinsert

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -1,29 +1,25 @@
 package piscine
 
-import "fmt"
-
 type TreeNode struct {
-	Left, Right, Parent *TreeNode
+	Parent, Right, Left *TreeNode
 	Data                string
 }
 
 func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 	for curr := root; curr != nil; {
-		fmt.Println("data: ", curr.Data)
 		if Compare(data, curr.Data) > 0 {
-		// if Compare(data, root.Data) > 0 {
 			if curr.Right != nil {
 				curr = curr.Right
 			} else {
 				curr.Right = &TreeNode{curr, nil, nil, data}
-				return curr
+				return curr.Right
 			}
 		} else {
 			if curr.Left != nil {
 				curr = curr.Left
 			} else {
 				curr.Left = &TreeNode{curr, nil, nil, data}
-				return curr
+				return curr.Left
 			}
 		}
 	}
