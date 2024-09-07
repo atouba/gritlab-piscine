a21aa9d4e4d693b55a0be4737482a02bac0cd5f6
atouba <anasstouba5@gmail.com>
2024-09-07 12:35:00 +0300

    search item*

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -3,9 +3,9 @@ package piscine
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	if root == nil {
 		return nil
-	} else if compare(root.Data, elem) > 0 {
+	} else if compare(elem, root.Data) > 0 {
 		return BTreeSearchItem(root.Right, elem)
-	} else if compare(root.Data, elem) < 0 {
+	} else if compare(elem, root.Data) < 0 {
 		return BTreeSearchItem(root.Left, elem)
 	} else {
 		return root
