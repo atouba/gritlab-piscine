0ba3d6223bb27117123a065389941de5840fe4c2
atouba <anasstouba5@gmail.com>
2024-09-09 07:45:01 +0300

    why the previous approach doesn't work

--- btreetransplant.go
+++ btreetransplant.go
@@ -1,6 +1,21 @@
 package piscine
 
+// func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
+// 	*node = *rplc
+// 	return root
+// }
+
+func isNodeLeft(node *TreeNode) bool {
+	return node == node.Parent.Left
+}
+
 func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
-	*node = *rplc
+	if root == node {
+		*root = *rplc
+	} else if isNodeLeft(node) {
+		node.Parent.Left = rplc
+	} else {
+		node.Parent.Right = rplc
+	}
 	return root
 }
